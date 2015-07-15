e1b8eacb672a00e964fd9c721534fe1efb672d4a
nobe1976 <charlesmc76@gmail.com>
2015-07-14 20:20:17 -0400

    Carbonite this Bitch

--- carbon.mk
+++ carbon.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# $(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d800/d800.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d800
-PRODUCT_NAME := cm_d800
+PRODUCT_NAME := carbon_d800
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D800
 PRODUCT_MANUFACTURER := lge
