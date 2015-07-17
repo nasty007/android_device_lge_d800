b68b928be2608c2b3119089b8adbad9cfb303de2
nobe1976 <charlesmc76@gmail.com>
2015-07-16 21:41:08 -0400

    Setup for DU

--- du.mk
+++ du.mk
@@ -1,25 +1,29 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common carbon stuff.
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+# Inherit some common du stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Enhanced NFC
-# $(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/d800/d800.mk)
+$(call inherit-product, device/lge/d800/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d800
-PRODUCT_NAME := carbon_d800
+PRODUCT_NAME := du_d800
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D800
 PRODUCT_MANUFACTURER := lge
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=lge/g2_att_us/g2:5.0.2/LRX22G/150722227a363:user/release-keys \
     PRIVATE_BUILD_DESC="g2_att_us-user 5.0.2 LRX22G 150722227a363 release-keys"
 
-PRODUCT_PACKAGES += Torch
+##PRODUCT_PACKAGES += Torch
